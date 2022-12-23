eedff967e021bc0e65f105cbc1e98f03a4051f07
Youri Janssen <kippenkiller555@gmail.com>
2022-12-23 10:17:27 +0100

    fasdf

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!fasdf');
 });
 
 app.listen(3001, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3001');
 });
+
+ 
