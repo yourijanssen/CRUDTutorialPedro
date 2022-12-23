85f298073121b624926afc25d897397feb296abe
Youri Janssen <kippenkiller555@gmail.com>
2022-12-23 11:17:09 +0100

    asdf

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [movieName, setMovieName] = React.useState('');
+  const [review, setReview] = React.useState('');
+
+
   return (
     <div className="App">
 
@@ -9,9 +14,13 @@ function App() {
 
       <div className='form'>
       <label>Movie Name:</label>
-      <input type="text" name="movieName"/>
+      <input type="text" name="movieName" onChange={(e) => {
+        setMovieName(e.target.value);
+      }}/>
       <label>Review:</label>
-      <input type="text" name="review" />
+      <input type="text" name="review" onChange={(e) => {
+        setReview(e.target.value);
+      }}/>
 
       <button>Submit</button>
       </div>    
--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const msql = require('mysql');
+
+const db = msql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'crud-database',
+  port: 3306
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!fasdf');
+
+  const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Inceptionnnn', 'Great movie!');";
+  db.query(sqlInsert, (err, result) => {
+    res.send('Hello World!fasgsdfgdgdsfgf'); npm
+  });
 });
+
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
 
- 
